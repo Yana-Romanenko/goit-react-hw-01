427728b32a0889d5a6ad17ab60cbd1f6fd290aea
Yana-Romanenko <yanaromanenko@ukr.net>
2024-02-18 11:55:56 +0200

    first task

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import styles from '../components/App.module.css'
+import Profile from "./Profile";
+import userData from '../userData.json'
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import styles from './Profile.module.css';
+export default Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={styles.profileContainer}>
+      <div>
+              <img className={styles.profileImage}
+                  src={image}
+                  alt="User avatar" />
+        <div className={styles.profileInfo}>
+          <p>{name}</p>
+          <p>@{tag}</p>
+          <p>{location}</p>
+        </div>
+      </div>
+      <ul className={styles.statsList}>
+        <li className={styles.statsListItem}>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li className={styles.statsListItem}>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li className={styles.statsListItem}>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
